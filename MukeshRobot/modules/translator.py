8072557673b68c9b00524a73a1fa84713fb8acf9
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 14:26:52 +0530

    Update translator.py

--- MukeshRobot/modules/translator.py
+++ MukeshRobot/modules/translator.py
@@ -59,7 +59,7 @@ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆
 """
 __mod_name__ = "⚡Tʀᴀɴs⚡"
 
