a8d086becd5b35a50210acdd3c8ac8084838fcdb
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 13:57:50 +0530

    Update rules.py

--- MukeshRobot/modules/rules.py
+++ MukeshRobot/modules/rules.py
@@ -160,7 +160,7 @@ __help__ = """
  ‣ `/setrules <your rules here>`*:* set the rules for this chat.
  ‣ `/clearrules`*:* clear the rules for this chat.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆
 """
 
 __mod_name__ = "⚡Rᴜʟᴇs⚡"
