c36d10d7982cf29cb3c2b05262370ba81608f235
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 14:43:57 +0530

    Update video.py

--- MukeshRobot/modules/video.py
+++ MukeshRobot/modules/video.py
@@ -110,4 +110,4 @@ __help_ = """
 /video to download video song
 /vsong to download video song 
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆"""
