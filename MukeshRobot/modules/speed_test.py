13dd232c904900779b488a45d3eba09472ac780d
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 14:16:38 +0530

    Update speed_test.py

--- MukeshRobot/modules/speed_test.py
+++ MukeshRobot/modules/speed_test.py
@@ -63,7 +63,7 @@ dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 __help__ = """
 » /speedtest *:* ʀᴜɴs ᴀ sᴘᴇᴇᴅᴛᴇsᴛ ᴀɴᴅ ᴄʜᴇᴄᴋ ᴛʜᴇ sᴇʀᴠᴇʀ sᴘᴇᴇᴅ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆
 """
 
 __mod_name__ = "⚡SᴘᴇᴇᴅTᴇsᴛ⚡​"
