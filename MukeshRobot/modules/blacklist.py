d69d659daf0b1bca255b7988bfa333fe1b7eeae9
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 08:59:50 +0530

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -464,7 +464,7 @@ Admin only:
  ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
  ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
