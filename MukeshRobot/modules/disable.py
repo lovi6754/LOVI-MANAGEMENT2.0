74dc059181c38bb23cb52db9f11462de842106f8
GODFATHER <104144725+mrtosumemon@users.noreply.github.com>
2023-04-29 09:15:02 +0530

    Update disable.py

--- MukeshRobot/modules/disable.py
+++ MukeshRobot/modules/disable.py
@@ -349,7 +349,7 @@ if is_module_loaded(FILENAME):
     ❍ /disablemodule <ᴍᴏᴅᴜʟᴇ ɴᴀᴍᴇ>*:* ᴅɪsᴀʙʟᴇ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴀᴛ ᴍᴏᴅᴜʟᴇ
     ❍ /listcmds*:* ʟɪsᴛ ᴀʟʟ ᴘᴏssɪʙʟᴇ ᴛᴏɢɢʟᴇᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁](https://t.me/your_godfather_xd)............☆
     """
 
     __mod_name__ = " ♨️Dɪsᴀʙʟᴇ♨️"
